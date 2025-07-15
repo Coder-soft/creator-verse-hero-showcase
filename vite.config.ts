2d97227844652a978b84fa45618bd76c920333c3
[dyad] <powernplant101@gmail.com>
2025-07-15 10:29:25 +0500

    [dyad] Fixing a react-dom module error - wrote 1 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ['react', 'react-dom', '@radix-ui/react-dialog', '@radix-ui/react-radio-group'],
+    dedupe: ['react', '@radix-ui/react-dialog', '@radix-ui/react-radio-group'],
   },
   optimizeDeps: {
     exclude: ['@radix-ui/react-dialog', '@radix-ui/react-radio-group'],
